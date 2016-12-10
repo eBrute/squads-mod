d00596e6ebba3538312536b54354b97d28a94296
Lass Safin <lasssafin@gmail.com>
2016-12-10 18:10:37 +0100

    SquadsMemberMixin.lua

--- output/lua/squads/Globals.lua
+++ output/lua/squads/Globals.lua
@@ -1,7 +1,9 @@
 kMaxSquads = 8
 kSquadInvalid = -1
 kMaxSquadsMembersPerSquad = 6
-kSquadType = enum (
+
+kSquadType = {
+	[0] = "Invalid",
 	"Red",
 	"Green",
 	"Blue",
@@ -10,4 +12,13 @@ kSquadType = enum (
 	"Yellow",
 	"Orange",
 	"Purple",
-);
+	Invalid = 0,
+	Red = 1,
+	Green = 2,
+	Blue = 3,
+	Cyan = 4,
+	Magenta = 5,
+	Yellow = 6,
+	Orange = 7,
+	Purple = 8
+};
