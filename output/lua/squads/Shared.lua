8f756c4c7deadcbfbbdee49ef91b9b9e37aeb538
Brute <email.brute@gmail.com>
2017-01-25 20:06:05 +0100

    remove unecessary checks

--- output/lua/squads/Shared.lua
+++ output/lua/squads/Shared.lua
@@ -3,7 +3,7 @@ local function OnDumpSquads()
         local teams = GetGamerules():GetTeams()
         for t = 1, #teams do
             local team = teams[t]
-            if team and HasMixin(team, "SquadTeam") then
+            if HasMixin(team, "SquadTeam") then
                 Log("Squads for team %d", teams[t].teamNumber)
                 teams[t]:DumpSquads()
             end
