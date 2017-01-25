bb18abed17e7a3e173e54d92adb0770277b2faca
Brute <email.brute@gmail.com>
2017-01-25 20:03:20 +0100

    better menu logic

--- output/lua/squads/Client.lua
+++ output/lua/squads/Client.lua
@@ -1,18 +1,36 @@
-local function ShowSquadSelectMenu()
-    gSquadSelect = GetGUIManager():CreateGUIScriptSingle("squads/GUISquadSelect")
-    gSquadSelect:SetIsVisible(true)
+local function CreatequadSelectMenu()
+    local player = Client.GetLocalPlayer()
+    if player:GetIsOnPlayingTeam() then
+        gSquadSelect = GetGUIManager():CreateGUIScriptSingle("squads/GUISquadSelect")
+        gSquadSelect:SetIsVisible(true)
+    end
 end
 
-local function HideSquadSelectMenu()
+local function DestroySquadSelectMenu()
     GetGUIManager():DestroyGUIScriptSingle("squads/GUISquadSelect")
     gSquadSelect = nil
 end
 
-local function ToggleSquadSelectMenu()
+local function ShowSquadSelectMenu()
     if not gSquadSelect then
-        ShowSquadSelectMenu()
-    else
+        CreatequadSelectMenu()
+    end
+    if gSquadSelect then
+        gSquadSelect:SetIsVisible(true)
+    end
+end
+
+local function HideSquadSelectMenu()
+    if gSquadSelect then
+        gSquadSelect:SetIsVisible(false)
+    end
+end
+
+local function ToggleSquadSelectMenu()
+    if gSquadSelect and gSquadSelect:GetIsVisible() then
         HideSquadSelectMenu()
+    else
+        ShowSquadSelectMenu()
     end
 end
 
@@ -20,7 +38,25 @@ Event.Hook("Console_squad_menu", ToggleSquadSelectMenu)
 Client.HookNetworkMessage("ShowSquadSelect", ShowSquadSelectMenu)
 
 
+local function GetIsPlayingTeam(teamNumber)
+    return teamNumber == kTeam1Index or teamNumber == kTeam2Index
+end
+
+local function ShowSquadSelectOnTeamChange(message)
+    if gSquadSelect then
+        DestroySquadSelectMenu()
+    end
+    if GetIsPlayingTeam(message.teamNumber) then
+        gSquadSelect = GetGUIManager():CreateGUIScriptSingle("squads/GUISquadSelect")
+        gSquadSelect:SetIsVisible(true)
+    end
+end
+
+Client.HookNetworkMessage("SetClientTeamNumber", ShowSquadSelectOnTeamChange)
+
+
 local function SelectSquad(squadNumber)
     Client.SendNetworkMessage("SelectSquad", {squadNumber = squadNumber}, true)
 end
+
 Event.Hook("Console_select_squad", SelectSquad)
