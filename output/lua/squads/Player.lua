b6da5c504dbc4ff5611cf093c30693a5177aa31d
Brute <email.brute@gmail.com>
2017-01-27 04:50:31 +0100

    sync squadnumber

--- output/lua/squads/Player.lua
+++ output/lua/squads/Player.lua
@@ -1,7 +1,10 @@
 Script.Load("lua/squads/SquadMemberMixin.lua")
+
 local oldPlayerOnCreate = Player.OnCreate
 
 function Player:OnCreate()
 	oldPlayerOnCreate(self)
 	InitMixin(self, SquadMemberMixin)
 end
+
+Shared.LinkClassToMap("Player", Player.kMapName, SquadMemberMixin.networkVars, true)
