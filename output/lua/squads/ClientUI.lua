ede353adbb686faef89f0b2b72a4eb295dff96d2
Brute <email.brute@gmail.com>
2017-02-05 04:33:32 +0100

    load waypoints for squadteams only

--- output/lua/squads/ClientUI.lua
+++ output/lua/squads/ClientUI.lua
@@ -1,2 +1,8 @@
-AddClientUIScriptForTeam(kTeam1Index, 'squads/GUISquadWaypoints')
-AddClientUIScriptForTeam(kTeam2Index, 'squads/GUISquadWaypoints')
+Script.Load("lua/squads/Globals.lua")
+
+if kSquadTeams[kTeam1Index] then
+    AddClientUIScriptForTeam(kTeam1Index, 'squads/GUISquadWaypoints')
+end
+if kSquadTeams[kTeam2Index] then
+    AddClientUIScriptForTeam(kTeam2Index, 'squads/GUISquadWaypoints')
+end
